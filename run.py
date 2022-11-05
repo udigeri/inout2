5da1b42dc64aae603fcdb1c938f2098707b05fae
Udi <udigeri@gmail.com>
2022-11-05 22:07:59 +0100

    initialize DB

--- run.py
+++ run.py
@@ -2,10 +2,17 @@ import argparse
 
 from inout2.web import WebApp
 from inout2.app import App
-from flask import render_template, request, session, flash, redirect, url_for
+from flask import render_template
+from flask import url_for
+from flask import redirect
+from flask import request
+from flask import session
+from flask import flash
+from flask import g
 
+import sqlite3
 
-inout2 = WebApp(debug=False)
+inout2 = WebApp(debug=True)
 flask_app = inout2.flask
 
 
@@ -14,36 +21,72 @@ flask_app = inout2.flask
 def view_home():
     return render_template("welcome.jinja")
 
+
+
 @flask_app.route("/login/", methods=["GET"])
 def view_login():
     return render_template("login.jinja")
 
+
+
 @flask_app.route("/login/", methods=["POST"])
-def check_login():
+def login_user():
     """Create session when correct user/password provided"""
-    error = None
     username = request.form['username']
     password = request.form['password']
-    if username and password:
-        rsp = inout2.getAuthentication(username, password)
-        if rsp:
-            error = rsp
-        else:
-            session['logged_in'] = True
-            flash('You were logged in', category='success')
-            return redirect(url_for('index'))
+    if username == "testexport" and password == "testexport":
+        session["logged"] = True
+        return redirect(url_for("view_admin"))
     else:
-        error = "Missing credentials"
+        return redirect(url_for("view_login"))
 
-    return render_template('login.jinja', error=error)
+
+
+@flask_app.route("/logout/", methods=["GET", "POST"])
+def logout_user():
+    """Drop session"""
+    session.pop("logged")
+    return redirect(url_for("view_home"))
+
+
 
 @flask_app.route("/admin/")
 def view_admin():
     return render_template("admin.jinja")
 
+
+
 @flask_app.route("/about/")
 def view_about():
     return render_template("about.jinja")
+
+
+
+## UTILS
+def connect_db():
+    rv = sqlite3.connect(inout2.DATABASE)
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def get_db():
+    if not hasattr(g, "sqlite_db"):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@flask_app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, "sqlite_db"):
+        g.sqlite_db.close()
+
+
+
+def init_db(app):
+    with app.app_context():
+        db = get_db()
+        with open("inout2/schema.sql", "r") as fp:
+            db.cursor().executescript(fp.read())
+        db.commit()
+
 
 
 
@@ -64,12 +107,18 @@ if (__name__ == "__main__"):
                                 action='store',
                                 default="production",
                                 help='Define conguration environment (default: %(default)s)')
-    parser.add_argument('-p', '--port', dest='web_port',
+    parser.add_argument('-i', '--init', dest='init_db',
                                 action='store',
-                                default="80",
-                                help='Define web server port (default: %(default)s)')
+                                required=False,
+                                help='Init database')
+
+
 
     app = App(__version__, parser.parse_args())
-    app.run(inout2)
-    app.finished()
-    
+    if (app.config.params.init_db != None):
+        init_db(inout2.flask)
+        app.logger.info("Initialize DB finished")
+
+    else:
+        app.run(inout2)
+        app.finished()
